92bf24267bb959ba262c68c3f81420ec881c02bc
Shivam Tiwari <shivam.t@appmocx.com>
2023-06-20 10:07:12 +0530

    user authentication

--- api/index.php
+++ api/index.php
@@ -1,18 +1,54 @@
-<!DOCTYPE html>
-<html>
-<body>
-
- 
-
-<h1>My first PHP pagee</h1>
-
- 
-
 <?php
-echo "Hello World!";
-?>
 
- 
+require '../../quiklrn_app/config.php';
+include '../../quiklrn_app/rester_api.php';
 
-</body>
-</html>
+// require 'external_api.php';
+header('Access-Control-Allow-Origin: *');
+
+$method_name = '';
+if (isset($_REQUEST['method']) && $_REQUEST['method'] != '') {
+    $method_name = $_REQUEST['method'];
+}
+
+switch ($method_name) {
+
+    case "test":
+        json_output(test());
+        break;
+
+    case "auth_function":
+        json_output(auth_function());
+        break;
+
+    }
+
+
+
+function auth_function()
+{
+    $auth = isset($_REQUEST['auth']) ? $_REQUEST['auth'] : '';
+
+    $myArray = [
+        "auth" => $auth,
+    ];
+    $aa_var = (form_rester_api_object("getUserIdByAuthCode", $myArray));
+    // var_dump($aa_var);die;
+  
+    $var = (json_encode([$aa_var]));
+    $headers = array('x-auth-key: ' . $auth);
+    $ab_var = APICall('POST', BASE_URL, $var, $headers);
+    $my_data = (json_decode($ab_var));
+    $data_arr['auth'] = array();
+    $getUserIdByAuthCode = $my_data->data->getUserIdByAuthCode;
+    if (count($my_data->data->getUserIdByAuthCode)) {
+        array_push($data_arr['auth'], $getUserIdByAuthCode);
+    }
+    return get_generic_response(200, $data_arr, "Records Found!");
+
+
+}
+
+
+
+
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,52 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div >
-      <h1>Hello Reactttt</h1>
-    </div>
+  const [posts, setPosts] = useState([]);
+  const [auth, setAuth] = useState([]);
+
+  var query = window.location.search;
+  var paramters = new URLSearchParams(query);
+  var auth_code = paramters.get('auth_code');
+  const params = useMemo(
+    () => ({
+      method: 'auth_function',
+      auth: auth_code,
+    }),
+    [auth_code]
   );
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/index.php', { params })
+      .then(response => {
+        let data = response.data.data;
+        let dataLength = Object.keys(data.auth).length;
+        if (!dataLength) {
+          alert('Invalid Auth!');
+          setAuth(false);
+          return;
+        } else {
+          alert('true');
+          setAuth(true);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, [params]);
+
+  return (
+    <div>
+      <h1>Conditional Rendering</h1>
+      {auth ? (
+        <p>You are logged in!</p>
+      ) : (
+        <p>You are not logged in.</p>
+      )}
+    </div>);
 }
 
 export default App;
