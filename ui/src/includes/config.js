f70cb4d33aeebb4ecc86f4de894967acedef50b2
shivam.t <shivam.t@appmocx.com>
2023-10-11 11:28:12 +0530

    passed test cases

--- ui/src/includes/config.js
+++ ui/src/includes/config.js
@@ -1,6 +1,7 @@
 // config.js
 
-const apiPath = 'http://localhost:8000/index.php';
+// const apiPath = 'http://localhost:8000/index.php';
 // const apiPath = 'https://meetings.quiklrn.com/api/index.php';
+const apiPath = 'https://liveconf-dev.quiklrn.net/liveconf/api/index.php';
 
 export default apiPath;
